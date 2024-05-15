a2af8dc9ca7de481da3484f063d69adc769bd3f1
AdityaSaiVarma <varma25072000@gmail.com>
2024-05-15 10:01:00 -0500

    Changes creating

--- Sample.java
+++ Sample.java
@@ -2,9 +2,11 @@ public class Sample
 {
      int a=10;
     static int b=20;
+    private int c=30;
     public static void main(String[] args){
         Sample s = new Sample();
         System.out.println(s.a);
         System.out.println(b);
+        System.out.println("Arg val is:"+args);
     }
 }
