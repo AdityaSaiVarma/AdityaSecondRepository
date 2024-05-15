0e0707ebb732bc1b101184291203b1beb2f5e648
AdityaSaiVarma <varma25072000@gmail.com>
2024-05-15 09:40:46 -0500

    Commited by adding

--- Sample.java
+++ Sample.java
@@ -1,7 +1,10 @@
 public class Sample
 {
      int a=10;
+    static int b=20;
     public static void main(String[] args){
         Sample s = new Sample();
-        System.out.println(s.a);}
+        System.out.println(s.a);
+        System.out.println(b);
+    }
 }
